de519325ea5b866d7c6fcaa53bb0ddee3baccf40
shunkat <shunkat@icloud.com>
2023-09-05 18:04:56 +0900

    firebaseとの接続ページ（仮）を作成

---
+++ lib/firebase_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirebasePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Firebase Page')),
+      body: Center(
+        child: Text('Firebase画面です。'),
+      ),
+    );
+  }
+}
